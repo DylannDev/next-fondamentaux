11fc7eaf6cbb177d05482e4247ea0df8bc097731
dylann97 <d.xavero@hotmail.com>
2024-11-07 13:31:13 +0100

    🚧 exercise done

--- src/app/exercise/about/page.exercise.tsx
+++ src/app/exercise/about/page.exercise.tsx
@@ -1,12 +1,15 @@
+"use client"
+
 const Page = () => {
   // 🐶 Détermine dans quel environnement s'exécute ce composant grâce à un log
-  // 🤖 `console.log('Je suis un développeur FullStack Next')`
+
   //
   // 🐶 Vérifie dans la console du navigateur et dans la console du serveur (terminal)
 
   // 🐶 N'oublie pas de faire les exercices bonus
+  console.log("Je suis un développeur FullStack Next")
   return (
-    <div>
+    <div onClick={() => alert("Hello")}>
       <p>Je suis un développeur FullStack Next</p>
     </div>
   )
